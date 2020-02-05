0d40bd9a545a91ec0fae819631443f230990f628
Hope <hopegiometti@gmail.com>
2020-02-05 12:30:28 -0500

    fixing routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :favorites
   resources :users
   resources :podcasts
+
+  root to: "podcasts#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
